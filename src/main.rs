e711a8f2601ad51459ca7f333a190cb64e1b8396
RayDarar <dfqgth400@gmail.com>
2024-07-14 16:28:44 +0500

    feat: rewrite to using serde

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 use std::env;
 
-use anyhow::{Context, Result};
-use axum::{routing::get, Router};
-use tokio::{net::TcpListener, signal};
+use actix_web::{get, web, App, HttpServer, Responder};
+use anyhow::Result;
 
-use tracing::{error, info, Level};
+use serde_json::json;
+use tracing::Level;
 use tracing_subscriber::FmtSubscriber;
 
-#[tokio::main]
+struct AppState {}
+
+#[actix_web::main]
 async fn main() -> Result<()> {
     let subscriber = FmtSubscriber::builder()
         .with_max_level(Level::TRACE)
@@ -15,33 +17,28 @@ async fn main() -> Result<()> {
     tracing::subscriber::set_global_default(subscriber)?;
 
     let port = match env::var("PORT") {
-        Ok(value) => value.parse::<i32>()?,
+        Ok(value) => value.parse::<u16>()?,
         Err(_) => 3000,
     };
 
-    tokio::spawn(serve_app(port));
+    let state = web::Data::new(AppState {});
 
-    info!(port, "Server is started");
-
-    match signal::ctrl_c().await {
-        Ok(()) => info!("Graceful shutdown"),
-        Err(err) => {
-            error!("Unable to listen for shutdown signal: {}", err);
-        }
-    }
+    HttpServer::new(move || {
+        App::new()
+            .app_data(state.clone())
+            .service(web::scope("/api").service(health))
+    })
+    .workers(4)
+    .bind(("127.0.0.1", port))?
+    .run()
+    .await?;
 
     Ok(())
 }
 
-async fn serve_app(port: i32) -> Result<()> {
-    let router = Router::new().route("/", get(|| async { "Hello, World!" }));
-
-    let app = Router::new().nest("/api", router);
-
-    let listener = TcpListener::bind(format!("0.0.0.0:{}", port))
-        .await
-        .with_context(|| format!("Failed to open listener on port {}", port))?;
-    axum::serve(listener, app).await?;
-
-    Ok(())
+#[get("/health")]
+async fn health() -> impl Responder {
+    web::Json(json!({
+        "status": true
+    }))
 }
